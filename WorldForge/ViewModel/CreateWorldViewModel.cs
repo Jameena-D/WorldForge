6302a838a8d4ec28fff36cf25cdc31a861da5847
Jameena <s1213831@student.windesheim.n>
2026-04-20 00:11:23 +0200

    A little fix to make sure no blank data can be saved by using enter, etc.

--- WorldForge/ViewModel/CreateWorldViewModel.cs
+++ WorldForge/ViewModel/CreateWorldViewModel.cs
@@ -8,10 +8,12 @@ namespace WorldForge.ViewModel
     {
         //T3 : Maxlength and required validation for the name and description fields, and required validation for the world type field. This ensures that the data submitted by the user meets the necessary criteria before being processed.
         [Required(ErrorMessage = "Name is required.")]
+        [RegularExpression(@"[\s\S]*\S[\s\S]*", ErrorMessage = "Name cannot be blank.")]
         [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
         public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Description is required.")]
+        [RegularExpression(@"[\s\S]*\S[\s\S]*", ErrorMessage = "Description cannot be blank.")]
         [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
         public string Description { get; set; } = string.Empty;
 
