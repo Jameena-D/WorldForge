ebc21be4db9140f33cc28cca91e0de139f382ccc
Jameena <s1213831@student.windesheim.n>
2026-05-10 12:15:19 +0200

    * ViewModels for login, register, sessions and change password

---
+++ WorldForge/ViewModel/UsersessionsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WorldForge.ViewModel
+{
+    public class UserSessionViewModel
+    {
+        public bool IsSignedIn { get; set; }
+        public string Username { get; set; }
+    }
+}
