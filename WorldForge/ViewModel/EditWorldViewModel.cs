e5f3cf75d8a8584ed3337799a6f1a888361b6e46
Jameena <s1213831@student.windesheim.n>
2026-05-16 18:40:14 +0200

    *Added the view (empty) to edit world * Made the ViewModel

---
+++ WorldForge/ViewModel/EditWorldViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WorldForge.ViewModel
+{
+    public class EditWorldViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string WorldType { get; set; }
+        public bool IsPublic { get; set; }
+        public List<WorldSectionInputViewModel> Sections { get; set; } = new();
+    }
+}
