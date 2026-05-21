43643c605d989810e940b4dc020cf388fedc5725
Jameena <s1213831@student.windesheim.n>
2026-05-21 12:33:46 +0200

    * Model and DTO for the report of a comment

---
+++ RestApi/Models/ReportedComment.cs
@@ -0,0 +1,15 @@
+﻿namespace RestApi.Models
+{
+    public class ReportedComment
+    {
+        public int Id { get; set; }
+        public int CommentId { get; set; }
+        public Comment Comment { get; set; } = null!;
+        public string ReportedUserId { get; set; } = string.Empty;
+
+        public Users ReportedUser { get; set; } = null!;
+        public string Reason {  get; set; } = string.Empty;
+        public DateTime ReportedAt { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ Shared/DTO/DTOReportedComment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.DTO
+{
+    public class DTOReportedComment
+    {
+        public int CommentId { get; set; }
+        public string ReportedUserId { get; set; } = string.Empty;
+        public string Reason { get; set; } = string.Empty;
+        public DateTime ReportedAt { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+
+    public class MuteUserDto
+    {
+        public string UserId { get; set; } = string.Empty;
+    }
+}
